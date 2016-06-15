c0d0a08f784b0fe501fa62c4ee431f6f51208c6f
Mikołaj Kromka <mkromka@virtuslab.com>
2016-06-15 16:04:14 +0200

    TPA #4 rKleene partial solution

--- src/main/scala/services/RKleene.scala
+++ src/main/scala/services/RKleene.scala
@@ -11,7 +11,22 @@ object RKleene extends ShortestPathServiceLike {
     * @return shortest paths matrix
     */
   override def computeShortestPaths(m: Matrix): Matrix = {
-    ??? // TODO TPA
+    if (m.size == 1) {
+      Matrix(Array(Array(0)))
+    } else if (m.size == 2){
+      m
+    } else {
+      val a = computeShortestPaths(m.getA)
+      val b = m.getB + a * m.getB
+      val c = m.getC + m.getC * a
+      val d = m.getD + c * b
+      val d2 = computeShortestPaths(d)
+      val b2 = b + b * d2
+      val c2 = c + d2 * c
+      val a2 = a + b2 * c2
+
+      Matrix(a=a2, b=b2, c=c2, d=d2)
+    }
   }
 
 }
